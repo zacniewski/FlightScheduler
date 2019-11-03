7e07ce120be2ac874476b4b3f06a0ce5c4f364fb
Artur Zacniewski <a.zacniewski@gmail.com>
2019-11-03 21:17:46 +0100

    Add DRF

--- backend/flights/views.py
+++ backend/flights/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, Artur!</h1>")
+    return HttpResponse("<h1>Hello, Flight Scheduler!</h1>")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    query = User.objects.all()
+    serialize_class = UserSerializer
--- backend/flightscheduler/settings.py
+++ backend/flightscheduler/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flights.apps.FlightsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
