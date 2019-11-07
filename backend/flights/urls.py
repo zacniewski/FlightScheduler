80386982ba78ec882db32f676a2b60b6ab99443f
Artur Zacniewski <a.zacniewski@gmail.com>
2019-11-07 20:56:03 +0100

    Create API endpoints

--- backend/flights/urls.py
+++ backend/flights/urls.py
@@ -1,11 +1,14 @@
+from django.conf.urls import url
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     # path('', views.index, name='index')
-    path('', include(router.urls))
+    # path('', include(router.urls))
+    url(r'^$', views.flight_list),
+    url(r'^(?P<pk>[0-9]+)$', views.flight_detail)
 ]
--- backend/flights/views.py
+++ backend/flights/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from .serializers import UserSerializer
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status, viewsets
+from rest_framework.parsers import JSONParser
+
+from .models import Schedule
+from .serializers import UserSerializer, ScheduleSerializer
 
 
 def index(request):
@@ -12,3 +16,56 @@ def index(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+# APIS
+# /flights
+@csrf_exempt
+def flight_list(request):
+    # Get all
+    if request.method == 'GET':
+        schedules = Schedule.objects.all()
+        schedules_serializer = ScheduleSerializer(schedules, many=True)
+        return JsonResponse(schedules_serializer.data, safe=False)
+
+    # Add one
+    if request.method == 'POST':
+        schedule_data = JSONParser().parser(request)
+        schedule_serializer = ScheduleSerializer(data=schedule_data)
+        if schedule_serializer.is_valid():
+            schedule_serializer.save()
+            return JsonResponse(schedule_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(schedule_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        Schedule.objects.all().delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def flight_detail(request, pk):
+    try:
+        schedule = Schedule.objects.get(pk=pk)
+    except Schedule.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    # get one
+    if request.method == 'GET':
+        schedule_serializer = ScheduleSerializer(schedule)
+        return JsonResponse(schedule_serializer.data)
+
+    # update one
+    if request.method == 'PUT':
+        schedule_data = JSONParser().parser(request)
+        schedule_serializer = ScheduleSerializer(schedule_data, data=schedule_data)
+        if schedule_serializer.is_valid():
+            schedule_serializer.save()
+            return JsonResponse(schedule_serializer.data)
+        return JsonResponse(schedule_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete one
+    if request.method == 'DELETE':
+        schedule.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
