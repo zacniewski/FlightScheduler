8155d3214197b59d83ce7a281a43bf0d848ce4e7
Artur Zacniewski <a.zacniewski@gmail.com>
2019-11-09 20:08:04 +0100

    FlightList Component

--- frontend/flight-scheduler/src/app/app.module.ts
+++ frontend/flight-scheduler/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
+import { FlightListComponent } from './flight-list/flight-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
