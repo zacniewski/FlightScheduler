cafd50ae923050779470904b24e86ce8fcc41313
Artur Zacniewski <a.zacniewski@gmail.com>
2019-11-09 20:07:43 +0100

    Correct corsheaders middleware

--- backend/flightscheduler/settings.py
+++ backend/flightscheduler/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 CORS_ORIGIN_WHITELIST = [
