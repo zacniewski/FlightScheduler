90e88d55981425da700d4d9a3468b9aded8b78c8
Artur Zacniewski <a.zacniewski@gmail.com>
2019-11-10 20:45:22 +0100

    Routing for flights

--- frontend/flight-scheduler/src/app/app-routing.module.ts
+++ frontend/flight-scheduler/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlightListComponent} from "./flight-list/flight-list.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'flights', pathMatch: 'full'},
+  {path: 'flights', component: FlightListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
